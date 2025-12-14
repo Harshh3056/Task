72415e5695acabe97eb553af02cc8eac828d1ed8
Harsh <harsh30563056@gmail.com>
2025-12-14 17:43:31 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import register_view
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('home', views.home, name='home'),
     path('quizzes/', views.quiz_list, name='quiz_list'),
     path('quiz/<int:pk>/', views.quiz_attempt, name='quiz_attempt'),
     path('result/<int:pk>/', views.result, name='result'),
     path('events/', views.events, name='events'),
+    path('register/', register_view, name='register'),
 ]
+
